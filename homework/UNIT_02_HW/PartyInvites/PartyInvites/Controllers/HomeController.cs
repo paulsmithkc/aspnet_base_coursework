f2f3d6e023c063f6c17ac9e79734e28423b3abb4
Paul Smith <prsmith@ranken.edu>
2019-08-27 14:13:04 -0500

    My First App

---
+++ homework/UNIT_02_HW/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+  public class HomeController : Controller
+  {
+    // GET: Home
+    public ActionResult Index()
+    {
+      int hour = DateTime.Now.Hour;
+      ViewBag.Hour = hour;
+      ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+      ViewBag.Email = "test@example.com";
+      return View();
+    }
+  }
+}
